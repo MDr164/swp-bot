74c35321d2b343fd804d8b3a3a6d94fad8f333a1
Marvin Drees <marvin@ceres-sys.de>
2021-03-20 21:20:37 +0100

    Add Nullcheck to prevent false positive
    ReviewTimer had a false positive when a PR was open but reviews where done

--- messages.go
+++ messages.go
@@ -79,20 +79,22 @@ func ReviewTimer(session *discordgo.Session, event bitbucketserver.PullRequestOp
 					}
 				}
 			}
-			r := rand.Intn(100)
-			switch {
-			case r <= 1:
-				_, err = session.ChannelMessageSend(cfg["PING_CHANNEL"], "https://media.discordapp.net/attachments/804343772695363624/821001536050167818/bananacover.png")
-			case r > 1 && r <= 25:
-				_, err = session.ChannelMessageSend(cfg["PING_CHANNEL"], "https://tenor.com/view/mai-nichijou-rain-glasses-gif-4749199")
-			case r > 25 && r <= 50:
-				_, err = session.ChannelMessageSend(cfg["PING_CHANNEL"], "https://tenor.com/view/nichijou-ona2000-anime-raining-rain-gif-13273260")
-			case r > 50 && r <= 75:
-				_, err = session.ChannelMessageSend(cfg["PING_CHANNEL"], "https://tenor.com/view/nichijou-yuuko-rain-gif-4749145")
-			case r > 75:
-				_, err = session.ChannelMessageSend(cfg["PING_CHANNEL"], "https://tenor.com/view/nichijou-rain-disappointed-anime-gif-9762544")
+			if body != "" {
+				r := rand.Intn(100)
+				switch {
+				case r <= 1:
+					_, err = session.ChannelMessageSend(cfg["PING_CHANNEL"], "https://media.discordapp.net/attachments/804343772695363624/821001536050167818/bananacover.png")
+				case r > 1 && r <= 25:
+					_, err = session.ChannelMessageSend(cfg["PING_CHANNEL"], "https://tenor.com/view/mai-nichijou-rain-glasses-gif-4749199")
+				case r > 25 && r <= 50:
+					_, err = session.ChannelMessageSend(cfg["PING_CHANNEL"], "https://tenor.com/view/nichijou-ona2000-anime-raining-rain-gif-13273260")
+				case r > 50 && r <= 75:
+					_, err = session.ChannelMessageSend(cfg["PING_CHANNEL"], "https://tenor.com/view/nichijou-yuuko-rain-gif-4749145")
+				case r > 75:
+					_, err = session.ChannelMessageSend(cfg["PING_CHANNEL"], "https://tenor.com/view/nichijou-rain-disappointed-anime-gif-9762544")
+				}
+				_, err = session.ChannelMessageSend(cfg["PING_CHANNEL"], body)
 			}
-			_, err = session.ChannelMessageSend(cfg["PING_CHANNEL"], body)
 		}
 	} else {
 		log.Println(err)
